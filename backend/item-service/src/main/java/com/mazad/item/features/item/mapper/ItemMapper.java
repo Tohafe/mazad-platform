a45a0d3033da0c3593f5f4a7312e5aacf6550824
HamzaARS2 <hamza1arrastec@gmail.com>
2025-12-28 16:42:17 +0100

    feat(item): ItemMapper

---
+++ backend/item-service/src/main/java/com/mazad/item/features/item/mapper/ItemMapper.java
@@ -0,0 +1,39 @@
+package com.mazad.item.features.item.mapper;
+
+import com.mazad.item.features.item.dto.ItemRequest;
+import com.mazad.item.features.item.dto.ItemResponse;
+import com.mazad.item.features.item.entity.ItemEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemMapper {
+
+
+    public ItemEntity toEntity(ItemRequest itemRequest) {
+        if (itemRequest == null)
+            return null;
+        return ItemEntity.builder()
+                .title(itemRequest.title())
+                .description(itemRequest.description())
+                .startingPrice(itemRequest.startingPrice())
+                .startsAt(itemRequest.startsAt())
+                .endsAt(itemRequest.endsAt())
+                .build();
+    }
+
+    public ItemResponse toResponse(ItemEntity entity) {
+        if (entity == null)
+            return null;
+        return new ItemResponse(
+                entity.getId(),
+                entity.getSellerId(),
+                entity.getTitle(),
+                entity.getDescription(),
+                entity.getStatus(),
+                entity.getStartingPrice(),
+                entity.getCurrentBid(),
+                entity.getStartsAt(),
+                entity.getEndsAt()
+        );
+    }
+}
