d77d914fec9125b6f9d206945ce1b3bbe7b52412
HamzaARS2 <hamza1arrastec@gmail.com>
2025-12-30 15:48:44 +0100

    feat(item): add getItemById endpoint

--- backend/item-service/src/main/java/com/mazad/item/features/item/api/ItemController.java
+++ backend/item-service/src/main/java/com/mazad/item/features/item/api/ItemController.java
@@ -5,13 +5,15 @@ import com.mazad.item.features.item.dto.ItemResponse;
 import com.mazad.item.features.item.service.ItemService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/v1/items")
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/items")
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
@@ -20,6 +22,12 @@ public class ItemController {
     public ResponseEntity<ItemResponse> create(@RequestBody @Valid ItemRequest itemRequest) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(itemService.createItem(itemRequest, 101L));
+                .body(itemService.createItem(itemRequest, UUID.randomUUID()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemResponse> getItemById(@PathVariable Long id) {
+        ItemResponse itemResponse = itemService.getItem(id);
+        return ResponseEntity.ofNullable(itemResponse);
     }
 }
