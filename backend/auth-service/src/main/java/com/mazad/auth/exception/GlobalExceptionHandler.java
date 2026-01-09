9779202baf79892c6c558b792dbcc79e3bdc3ab6
rekoune <rekoune123@gmail.com>
2026-01-09 03:01:08 +0100

    feat(auth): add the GlobalExceptionHandler and DuplicateResourceException

---
+++ backend/auth-service/src/main/java/com/mazad/auth/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.mazad.auth.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ProblemDetail duplicateResourceException(DuplicateResourceException e){
+        return ProblemDetail
+                        .forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail validationExceptionHandler(MethodArgumentNotValidException e){
+        return ProblemDetail
+                        .forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ProblemDetail runTimeExceptionHandler(Exception e){
+        return ProblemDetail
+                        .forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+}
