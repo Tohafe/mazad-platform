76eb9e4f0a5b0903e0983b4f04f1c27dad242343
rekoune <rekoune123@gmail.com>
2026-01-09 15:34:45 +0100

    feat(auth): add the function getUserById and work with mapper in the function addUser

--- backend/auth-service/src/main/java/com/mazad/auth/service/UserService.java
+++ backend/auth-service/src/main/java/com/mazad/auth/service/UserService.java
@@ -1,6 +1,7 @@
 package com.mazad.auth.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,7 @@ import com.mazad.auth.dto.UserRequestDTO;
 import com.mazad.auth.dto.UserResponseDTO;
 import com.mazad.auth.entity.UserEntity;
 import com.mazad.auth.exception.DuplicateResourceException;
+import com.mazad.auth.exception.UserNotFoundException;
 import com.mazad.auth.mapper.UserMapper;
 import com.mazad.auth.repo.UserRepo;
 
@@ -19,7 +21,7 @@ public class UserService {
     private  final  UserRepo repo;
     private  final  UserMapper mapper;
 
-    public List<UserResponseDTO> getAll(){
+    public List<UserResponseDTO> getAllUsers(){
         List<UserResponseDTO> users;
 
         users = repo.findAll()
@@ -29,26 +31,23 @@ public class UserService {
         return users;
     }
 
+    public UserResponseDTO getUserById(UUID id){
+        UserEntity user = repo
+                            .findById(id)
+                            .orElseThrow(() -> new UserNotFoundException());
+        return mapper.toResponseDTO(user);
+        
+    }
+
     public UserResponseDTO addUser(UserRequestDTO userRequest) {
-        UserEntity user = UserEntity.builder()
-                                    .email(userRequest.email())
-                                    .password(userRequest.password())
-                                    .userName(userRequest.userName())
-                                    .firstName(userRequest.firstName())
-                                    .lastName(userRequest.lastName())
-                                    .build();
+        UserEntity user = mapper.toEntity(userRequest);
+
         if (repo.existsByEmail(user.getEmail()))
             throw new DuplicateResourceException("Email is already in use");
         else if (repo.existsByUserName(user.getUserName()))
             throw new DuplicateResourceException("Username is already taken");
             
         user = repo.save(user);
-        return UserResponseDTO.builder()
-                                .id(user.getId())
-                                .email(user.getEmail())
-                                .userName(user.getUserName())
-                                .firstName(user.getFirstName())
-                                .lastName(user.getLastName())
-                                .build();
+        return mapper.toResponseDTO(user);
     }
 }
