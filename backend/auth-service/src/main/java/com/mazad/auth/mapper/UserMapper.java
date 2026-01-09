3d31bfbfd4e516215784577032a57fee3f503e50
rekoune <rekoune123@gmail.com>
2026-01-09 23:08:01 +0100

    feat(auth): add SecurityConfig and hash the password before saving it to database

--- backend/auth-service/src/main/java/com/mazad/auth/mapper/UserMapper.java
+++ backend/auth-service/src/main/java/com/mazad/auth/mapper/UserMapper.java
@@ -1,19 +1,29 @@
 package com.mazad.auth.mapper;
  
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.mazad.auth.dto.UserRequestDTO;
 import com.mazad.auth.dto.UserResponseDTO;
 import com.mazad.auth.entity.UserEntity;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class UserMapper {
+
+    private final PasswordEncoder passwordEncoder;
+
     public UserEntity toEntity(UserRequestDTO request){
         if (request == null)
             return null;
+        String passwordHash;
+
+        passwordHash = passwordEncoder.encode(request.password());
         return UserEntity.builder()
                             .email(request.email())
-                            .password(request.password())
+                            .password(passwordHash)
                             .userName(request.userName())
                             .firstName(request.firstName())
                             .lastName(request.lastName())
