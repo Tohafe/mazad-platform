ec80a2c3ffe5f46dd54bca432d9f327e123a3240
rekoune <rekoune123@gmail.com>
2026-01-13 15:32:07 +0100

    feat(auth): implement basic authentication and security configuration

---
+++ backend/auth-service/src/main/java/com/mazad/auth/service/UserDetailService.java
@@ -0,0 +1,27 @@
+package com.mazad.auth.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mazad.auth.repo.UserRepo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepo repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repo
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Email Not found"));
+    }
+
+}
--- backend/auth-service/src/main/java/com/mazad/auth/service/UserService.java
+++ backend/auth-service/src/main/java/com/mazad/auth/service/UserService.java
@@ -26,7 +26,7 @@ public class UserService {
 
         users = repo.findAll()
                 .stream()
-                .map(entity -> mapper.toResponseDTO(entity))
+                .map(mapper::toResponseDTO)
                 .toList();
         return users;
     }
@@ -34,7 +34,7 @@ public class UserService {
     public UserResponseDTO getUserById(UUID id){
         UserEntity user = repo
                             .findById(id)
-                            .orElseThrow(() -> new UserNotFoundException());
+                            .orElseThrow(UserNotFoundException::new);
         return mapper.toResponseDTO(user);
         
     }
