04209e60444eb7425043cdd0698e204f9b9c9254
HamzaARS2 <hamza1arrastec@gmail.com>
2026-01-24 17:51:33 +0100

    fix(gateway): remove api key from rate limit test script

--- backend/mazad-gateway/rate_limit.sh
+++ backend/mazad-gateway/rate_limit.sh
@@ -1,8 +1,8 @@
 
 while [ 1 ]; do
-  curl localhost:8080/api/v1/items -H "X-API-KEY: c0221589-ca50-4518-9182-615460a3b241" -s -o /dev/null -w "%{http_code}"
+  curl localhost:8080/api/v1/items -s -o /dev/null -w "%{http_code}"
   echo -n " "
-  curl localhost:8080/api/v1/items -H "X-API-KEY: c0221589-ca50-4518-9182-615460a3b241" -s -o /dev/null -w "%{http_code}"
+  curl localhost:8080/api/v1/items -s -o /dev/null -w "%{http_code}"
   sleep 1
   echo ""
 done
--- backend/mazad-gateway/src/main/java/com/mazad/mazadgateway/config/GatewayConfig.java
+++ backend/mazad-gateway/src/main/java/com/mazad/mazadgateway/config/GatewayConfig.java
@@ -1,11 +1,7 @@
 package com.mazad.mazadgateway.config;
 
 
-import com.mazad.mazadgateway.filters.CommonFilters;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
-import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
