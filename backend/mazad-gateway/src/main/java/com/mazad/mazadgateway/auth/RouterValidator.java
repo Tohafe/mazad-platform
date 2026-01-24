54876b9781f1fae4ebcb1069c161e1c9f0625646
HamzaARS2 <hamza1arrastec@gmail.com>
2026-01-24 18:17:45 +0100

    feat(gateway): add item service path to the openApi end points

--- backend/mazad-gateway/src/main/java/com/mazad/mazadgateway/auth/RouterValidator.java
+++ backend/mazad-gateway/src/main/java/com/mazad/mazadgateway/auth/RouterValidator.java
@@ -8,16 +8,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RouterValidator {
-    private RouterValidator(){}
+    private RouterValidator() {
+    }
 
     public static final List<String> openApiEndPoints = List.of(
-        "/api/v1/auth/login",
-        "/api/v1/auth/register",
-        "/api/v1/auth/refresh"
+            "/api/v1/auth/login",
+            "/api/v1/auth/register",
+            "/api/v1/auth/refresh",
+            "/api/v1/items"
     );
 
-    public static final Predicate<ServerHttpRequest> isSecured = 
-                                request -> openApiEndPoints
-                                            .stream()
-                                            .noneMatch(uri -> request.getURI().getPath().contains(uri));
+    public static final Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndPoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
 }
