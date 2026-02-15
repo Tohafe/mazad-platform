325dc677d30aaff83682838ffd22596ddec43e0c
Abderrahman Jbari <ajbari@c2r5p8.1337.ma>
2026-02-15 21:50:58 +0000

    feat: create Message Data Model

--- backend/chat-service/src/main/java/com/mazad/chat_service/ChatServiceApplication.java
+++ backend/chat-service/src/main/java/com/mazad/chat_service/ChatServiceApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(ChatServiceApplication.class, args);
 		System.out.println("====================================");
 		System.out.println("Chat Service is running ...");
 		System.out.println("====================================");
-		SpringApplication.run(ChatServiceApplication.class, args);
 
 
 
