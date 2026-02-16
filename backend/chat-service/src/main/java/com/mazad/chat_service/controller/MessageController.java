dedd5c01d36c116257bcc9eae67e9b2219ddd07d
Abderrahman Jbari <ajbari@c2r7p10.1337.ma>
2026-02-16 17:21:30 +0000

    fix: fix critical typos in repositery and Message Model
    fix: fixed some typos in the annotations
    
    fix: fixe typos in repository keyword
    
    fix: rename repo class to Repository
    
    fix: fix critical typos in repositery and Message Model

--- backend/chat-service/src/main/java/com/mazad/chat_service/controller/MessageController.java
+++ backend/chat-service/src/main/java/com/mazad/chat_service/controller/MessageController.java
@@ -1,25 +1,34 @@
 package src.main.java.com.mazad.chat_service.controller;
 
-import com.mazad.chat_service.repository.MessageRepository;
+
+import src.main.java.com.mazad.chat_service.repository.MessageRepository;
+import src.main.java.com.mazad.chat_service.model.Message;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("api/chat")
 public class MessageController {
     
-    @Autouwired
+    @Autowired
     MessageRepository Repo;
 
-    @RequestMapping("/send")
+    @PostMapping("/send")
     public Message sendMessage(@RequestBody Message message) {
         // save this in repo by the save function ig 
 
-
-        Repo.save(message);
+        System.out.println("In Send RequestMapping");
+        return Repo.save(message);
     }
-
-    @RequetMapping("/receive/{userId}")
+    
+    @GetMapping("/receive/{userId}")
     public List<Message> receiveMessage(@PathVariable long userId)
     {
+        System.out.println("In post RequestMapping");
         return Repo.FindByreceiverId(userId);
     }
 
