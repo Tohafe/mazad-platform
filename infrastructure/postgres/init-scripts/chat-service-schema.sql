65ed628b72f2f7c2db01b76edd8f5e3f5c6769f2
Abderrahman Jbari <ajbari@c2r7p10.1337.ma>
2026-02-17 22:52:59 +0000

    feat: add room_id in database for fetching chat history more efficient

--- infrastructure/postgres/init-scripts/chat-service-schema.sql
+++ infrastructure/postgres/init-scripts/chat-service-schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE "chat-service-db";
 
 CREATE TABLE IF NOT EXISTS  messages (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    chat_id BIGINT NOT NULL,
+    room_id VARCHAR(50) NOT NULL,
     sender_id BIGINT NOT NULL,
     receiver_id BIGINT NOT NULL,
     content TEXT NOT NULL,
     timestamp  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP  NOT NULL
 );
-
+CREATE INDEX IF NOT EXISTS idx_messages_room_id  ON messages(room_id);
 CREATE INDEX IF NOT EXISTS idx_messages_sender ON messages(sender_id);
 CREATE INDEX IF NOT EXISTS idx_messages_receiver ON messages(receiver_id);
