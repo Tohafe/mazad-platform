174c7ebd2cab7877114da941f8f6202b4da3df79
Abderrahman Jbari <ajbari@c2r7p10.1337.ma>
2026-02-17 22:56:24 +0000

    feat: create fetchChatHistory func to fetch chat history with room_id, and saving message with the new room_id logic

--- backend/chat-service/src/main/java/com/mazad/chat_service/repository/MessageRepository.java
+++ backend/chat-service/src/main/java/com/mazad/chat_service/repository/MessageRepository.java
@@ -3,17 +3,29 @@ package com.mazad.chat_service.repository;
 import com.mazad.chat_service.model.Message;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-
+import org.springframework.data.jpa.repository.Query;
 
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, UUID>
 {
+    List<Message> findByRoomIdOrderByTimestampAsc(String roomId);
+    @Query(value = """
+            SELECT * FROM (
+                SELECT DISTINCT ON (room_id) *
+                FROM messages
+                WHERE receiver_id = :userId OR sender_id = :userId
+                ORDER BY room_id, timestamp DESC
+            ) sub
+            ORDER BY timestamp DESC
+            """, nativeQuery = true)
+    List<Message> findInbox(@Param("userId") long userId);
 
-    List<Message> findByChatId(long chatId);
+    // List<Message> findByChatId(long chatId);
     // List<Message> FindBySenderId(long senderId);
-    List<Message> findByReceiverId(long receiverId);
+    // List<Message> findByReceiverId(long receiverId);
     
 }
