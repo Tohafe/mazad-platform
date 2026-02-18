16146b0f10613df66ee47a4752c423a3aa4cd27b
Abderrahman Jbari <ajbari@c2r7p8.1337.ma>
2026-02-18 20:48:42 +0000

    feat: added kafka producer logic

--- backend/chat-service/src/main/java/com/mazad/chat_service/controller/MessageController.java
+++ backend/chat-service/src/main/java/com/mazad/chat_service/controller/MessageController.java
@@ -2,6 +2,10 @@ package com.mazad.chat_service.controller;
 
 
 import com.mazad.chat_service.repository.MessageRepository;
+import com.mazad.chat_service.service.MessageService;
+
+import lombok.extern.slf4j.Slf4j;
+
 import com.mazad.chat_service.model.Message;
 
 
@@ -9,29 +13,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-
+@Slf4j
 @RestController
 @RequestMapping("api/chat")
 public class MessageController {
     
     @Autowired
-    MessageRepository Repo;
-
+    MessageRepository repo;
+    @Autowired
+    MessageService service;
+    
     @PostMapping("/send")
     public Message sendMessage(@RequestBody Message message) {
         // save this in repo by the save function ig 
 
-        System.out.println("In Send RequestMapping");
-        return Repo.save(message);
+        log.info("In Send RequestMapping");
+        return service.sendMessage(message);
     }
     
-    @GetMapping("/receive/{userId}")
-    public List<Message> receiveMessage(@PathVariable long userId)
+    @GetMapping("/history/{otherUserId}")
+    public List<Message> fetchChatHistory(
+        @RequestHeader("X-user-Id") long myId,
+        @PathVariable long otherUserId)
     {
-        System.out.println("In post RequestMapping");
-        return Repo.findByReceiverId(userId);
+        log.info("In post RequestMapping");
+        log.info("Fetching history between " + myId + " and " + otherUserId);
+        return service.fetchChatHistory(myId, otherUserId);
     }
-
-
-    
+    // TODO : end point for fetching the chats 
+    // @GetMapping("/inbox/{userId}")
+    // public List<Message> fetchInboxChats(@PathVariable long userId)
+    // {
+    //     System.out.println("In post RequestMapping");
+    //     return service.fetchChatHistory(, userId);
+    // }
 }
