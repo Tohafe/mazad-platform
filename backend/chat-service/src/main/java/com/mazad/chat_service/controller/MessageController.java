67463d5925f801cd7671d2f0947b7fb8f6c13dfb
Abderrahman Jbari <ajbari@c2r7p8.1337.ma>
2026-02-19 17:57:39 +0000

    feat: add Slice logic, and plus adding and pageble and kafka error handling

--- backend/chat-service/src/main/java/com/mazad/chat_service/controller/MessageController.java
+++ backend/chat-service/src/main/java/com/mazad/chat_service/controller/MessageController.java
@@ -4,14 +4,19 @@ package com.mazad.chat_service.controller;
 import com.mazad.chat_service.repository.MessageRepository;
 import com.mazad.chat_service.service.MessageService;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 import com.mazad.chat_service.model.Message;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -23,22 +28,36 @@ public class MessageController {
     @Autowired
     MessageService service;
     
+
     @PostMapping("/send")
-    public Message sendMessage(@RequestBody Message message) {
-        // save this in repo by the save function ig 
+    public Message sendMessage(
+            @RequestHeader("X-User-Id") long myId,
+            @Valid @RequestBody Message message) {
+            // save this in repo by the save function ig 
 
         log.info("In Send RequestMapping");
-        return service.sendMessage(message);
+        return service.sendMessage(message, myId);
     }
     
     @GetMapping("/history/{otherUserId}")
-    public List<Message> fetchChatHistory(
+    public Slice<Message> fetchChatHistory(
         @RequestHeader("X-user-Id") long myId,
-        @PathVariable long otherUserId)
+        @PathVariable long otherUserId,
+        @PageableDefault(
+            size = 2,
+            sort = "timestamp",
+            direction = Sort.Direction.DESC
+        )
+        Pageable pageable)
     {
+
+        if (pageable.getPageSize() > 5){
+            pageable = PageRequest.of(pageable.getPageNumber(), 5, pageable.getSort());
+        }
+
         log.info("In post RequestMapping");
         log.info("Fetching history between " + myId + " and " + otherUserId);
-        return service.fetchChatHistory(myId, otherUserId);
+        return service.fetchChatHistory(myId, otherUserId, pageable);
     }
     // TODO : end point for fetching the chats 
     // @GetMapping("/inbox/{userId}")
--- backend/chat-service/src/main/java/com/mazad/chat_service/service/MessageService.java
+++ backend/chat-service/src/main/java/com/mazad/chat_service/service/MessageService.java
@@ -5,25 +5,13 @@ import  com.mazad.chat_service.repository.MessageRepository;
 import lombok.extern.slf4j.Slf4j;
 
 import  com.mazad.chat_service.model.Message;
-
-import org.hibernate.type.descriptor.jdbc.JavaTimeJdbcType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.beans.factory.annotation.Value;
-
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.annotation.*;
-
-
-import org.springframework.kafka.support.SendResult;
-
-// import com.mazad.chat_service.infrastructure.kafka.SendMessageProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mazad.chat_service.infrastructure.kafka.ChatEventProducer;
 
 @Slf4j
 @Service 
@@ -32,26 +20,14 @@ public class MessageService {
     @Autowired
     MessageRepository repository;
 
-
     @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
-
-    private final JsonMapper jsonMapper;
-    public MessageService(){
-        this.jsonMapper = new JsonMapper();
-        this.jsonMapper.registerModule(new JavaTimeModule());
-        this.jsonMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    }
+    ChatEventProducer chatEventProducer;
 
 
 
-
-    @Value("${chat.kafka.topic}")
-    private String topicName;
-
-
-    public Message sendMessage(Message message)
+    public Message sendMessage(Message message, long myId)
     {
+        message.setSenderId(myId);
         long minId = Math.min(message.getSenderId(), message.getReceiverId());
         long maxId = Math.max(message.getSenderId(), message.getReceiverId());
         String roomId = minId + "_" + maxId;
@@ -60,29 +36,10 @@ public class MessageService {
 
         Message savedMessage = repository.save(message);
 
-
         try {
-            String jsonPayload = jsonMapper.writeValueAsString(savedMessage);
-            CompletableFuture<SendResult<String, String>> completableFuture = kafkaTemplate.send(topicName, roomId, jsonPayload);
-            completableFuture.whenComplete(
-                (result, ex) -> {
-                    if (ex == null)
-                    {
-                        log.info("√: Kafka sent succefully: Topic: {}, Partition: {}, offset: {}", 
-                            topicName,
-                            result.getRecordMetadata().partition(),
-                            result.getRecordMetadata().offset());
-                    }
-                    else {
-                        log.error("X Kafka faild sending: message ID: {}. Error: {} ",
-                            savedMessage.getId(),
-                            ex.getMessage());
-
-                    }
-                }
-            );
+            chatEventProducer.sendMessageEvent(savedMessage);
         }
-        catch (Exception e)
+        catch (JsonProcessingException e)
         {
             log.error("XXXXXXXX preparing kafka error: {} ! XXXXXXXXX\n", e.getMessage());
             e.printStackTrace();
@@ -90,13 +47,13 @@ public class MessageService {
         return savedMessage;
     }
 
-    public List<Message> fetchChatHistory(long userId1, long userId2){
+    public Slice<Message> fetchChatHistory(long userId1, long userId2, Pageable pageable){
         
         long minId = Math.min(userId1, userId2);
         long maxId = Math.max(userId1, userId2);
 
         String roomId   = minId + "_" + maxId;
-        return repository.findByRoomIdOrderByTimestampAsc(roomId);
+        return repository.findByRoomIdOrderByTimestampAsc(roomId, pageable);
     }
 
     
