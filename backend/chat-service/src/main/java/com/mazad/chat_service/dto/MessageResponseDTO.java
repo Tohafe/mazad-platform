2f649a2eaeaf46a616657e6935bfccd7411c1c95
Abderrahman Jbari <ajbari@c2r7p8.1337.ma>
2026-02-20 17:54:24 +0000

    feat: add DTO for Message returnin in send/ request and for in /history request, and addin exception Handling

---
+++ backend/chat-service/src/main/java/com/mazad/chat_service/dto/MessageResponseDTO.java
@@ -0,0 +1,19 @@
+package com.mazad.chat_service.dto;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class MessageResponseDTO {
+    private UUID    id;
+    private long    senderId;
+    private String    content;
+    private ZonedDateTime timestamp;    
+}
---
+++ backend/chat-service/src/main/java/com/mazad/chat_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.mazad.chat_service.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String> > handleValidationException(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<Map<String,String> > handleMissingHeader(MissingRequestHeaderException ex){
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Missing Required Header: " + ex.getHeaderName());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String,String> > handleIllegalArgument(IllegalArgumentException ex){
+        Map<String, String> error = new HashMap<>();
+
+        error.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String> > handleGenericException(Exception ex){
+        Map<String, String> error = new HashMap<>();
+        error.put("Error", "Internal Server Error");
+        log.error("Critical Server Error ", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+}
