b0daeca7f72de7a7c19c299c07236f1f75d285cc
AOUKY <aoukyhamza@gmail.com>
2026-02-27 10:00:09 +0000

    fix: remove test code

--- backend/notification-service/src/main/java/com/mazad/notification/controller/NotificationController.java
+++ backend/notification-service/src/main/java/com/mazad/notification/controller/NotificationController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.mazad.notification.service.NotificationService;
@@ -21,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/notifications")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+// @CrossOrigin(origins = "http://localhost:5173")
 public class NotificationController {
 
     private final NotificationService notifactionService;
@@ -30,7 +30,7 @@ public class NotificationController {
     public ResponseEntity<Slice<NotificationEntity>> getUnreadNotifications(@RequestHeader("X-User-Id") String userId,
                                                                         @RequestParam(defaultValue = "20") int pageSize,
                                                                         @RequestParam(defaultValue = "0" ) int pageNumber) {
-                                                                            userId = "01";
+                                                                            // userId = "01";
                                                                             return ResponseEntity.ok(notifactionService.getUnreadNotification(userId, pageNumber, pageSize)) ;
                                                                         }
                                                                         
@@ -38,7 +38,7 @@ public class NotificationController {
     public ResponseEntity<Slice<NotificationEntity>> getNotifications(@RequestHeader("X-User-Id") String userId,
                                                                         @RequestParam(defaultValue = "20") int pageSize,
                                                                         @RequestParam(defaultValue = "0" ) int pageNumber){
-                                                                            userId = "01";
+                                                                            // userId = "01";
         return ResponseEntity.ok(notifactionService.getNotifications(userId, pageNumber, pageSize));
 
     }
