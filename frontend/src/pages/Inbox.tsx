b1d1166d34df4f11309163dfc1e75a023e5af334
Abderrahman Jbari <ajbari@c2r7p3.1337.ma>
2026-02-27 22:24:11 +0000

    feat: add ConversationList component integeration in Inbox.tsx

--- frontend/src/pages/Inbox.tsx
+++ frontend/src/pages/Inbox.tsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
-
-
+import { act, useState } from "react";
+import ConversationList from "../components/Chat/ConversationList";
+import type { Chat } from "../components/Chat/ConversationList";
+import ChatWindow from "../components/Chat/ChatWindow";
 
 
 function Inbox(){
 
     const [activeChatId, setActiveChatId] = useState<number | null>(null);
 
-    const fakeChats = [
-        {id: 1, name: "Hamza", lastMessage: "Whats the last price ?"},
-        {id: 2, name: "Ahmed", lastMessage: "Is this item still on Mazad ?"}
+    const fakeChats : Chat[]=
+    [
+        {id: 1, name: "Hamzam", lastMessage: ""},
+        {id: 3, name: "Hamza", lastMessage: "Can i get the full history of the Item ?"},
+        {id: 4, name: "Hamza", lastMessage: "Can i get the full history of the Item ?"},
+        // {id: 5, name: "Hamza", lastMessage: "Can i get the full history of the Item ?"},
+        // {id: 6, name: "Hamza", lastMessage: "Can i get the full history of the Item ?"},
+        // {id: 7, name: "Hamza", lastMessage: "Can i get the full history of the Item ?"},
+        // {id: 8, name: "Hamza", lastMessage: "Can i get the full history of the Item ?"},
+        // {id: 9, name: "Hamza", lastMessage: "Can i get the full history of the Item ?"},
+        // {id: 0, name: "Hamza", lastMessage: "Can i get the full history of the Item ?"},
+        // {id: 2, name: "Ahmed", lastMessage: "Is this item still on Mazad ?"}
 
     ];
     
@@ -20,39 +30,38 @@ function Inbox(){
             {/* MAIN INBOX CONTAINER */}
             <div className="flex w-full max-w-7xl h-[calc(100vh-220px)] min-h-150 bg-white border border-gray-300 rounded-lg shadow-sm overflow-y-hidden">
                 {/* Left panel */}
-                <div className="w-100 flex flex-col border-r border-gray-300">
+                <div className="w-96  flex flex-col border-r border-gray-300">
                     {/* header area for the left panel */}
                     <div className=" p-6 border-b border-gray-300">
                         <h2 className="text-2xl font-bold">Messages</h2>
                     </div>
                     
                     {/* CONVERSTAION LIST */}
-                    <div className="flex flex-1 overflow-y-auto">
-                        <p className="p-6 text-center text-sm text-gray-500">
-                            (ConversationList components would here)
-                        </p>
-                    </div>
+                    <ConversationList 
+                        chats={fakeChats}
+                        activeChatId={activeChatId}
+                        onSelectChat={setActiveChatId}
+                    />
+
 
                 </div>
                 {/* Right Panel */}
                 <div className="flex-1 flex flex-col bg-white">
                     {(activeChatId) ? (
-                            <div className="flex flex-1 items-center justify-center text-xl font-semibold text-gray-700">
-                                Chatting with ID: {activeChatId}
+                        <ChatWindow chatId={activeChatId}/>
+                    ) : (
+                        <div className="flex-1 flex flex-col items-center justify-center text-gray-500 gap-4">
+                            <div className="w-24 h-24 rounded-full mb-2  bg-blue-50 flex items-center justify-center text-blue-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+                                </svg>
                             </div>
+                            <h3 className="text-xl font-semibold text-gray-800">Start a conversation</h3>
+                            <p className="text-sm">Select a chat on the left to see your messages here.</p>
+                        </div>
+                    )}
 
-                        ) : (
-                            <div className="flex-1 flex flex-col items-center justify-center text-gray-500 gap-4">
-                                <div className="w-24 h-24 rounded-full mb-2  bg-blue-50 flex items-center justify-center text-blue-500">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-                                    </svg>
-                                </div>
-                                <h3 className="text-xl font-semibold text-gray-800">Start a conversation</h3>
-                                <p className="text-sm">Select a chat on the left to see your messages here.</p>
-                            </div>
 
-                        )}
 
                 </div>
 
