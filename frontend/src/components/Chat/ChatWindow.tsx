cf2dceb67b79635b123558bd6220cc897819c137
Abderrahman Jbari <ajbari@c2r7p3.1337.ma>
2026-03-03 17:46:17 +0000

    feat: adding messages sending logic to /send endpoint

--- frontend/src/components/Chat/ChatWindow.tsx
+++ frontend/src/components/Chat/ChatWindow.tsx
@@ -1,26 +1,87 @@
+import {  useEffect, useRef, useState } from "react";
+import useApiPrivate from "../../hooks/useApiPrivate";
+import { useAuth } from "../../context/AuthProvider"
 
 
 
-function ChatWindow({ chatId } : {chatId:number}){
+function ChatWindow({ chatId } : Readonly<{chatId:string}>){
 
-    const fakeMessages = [
-        { id: 1, text: "Hey! Is the auction got postponedHey! Is the auction got postponedHey! Is the auction got postponedHey! Is the auction got postponed ?", sender: "them"},
-        { id: 2, text: "Yes it is postponed, are you interested ? ", sender: "me"},
-        { id: 2, text: "Yes it is postponed, are you interested ? ", sender: "them"},
-        { id: 2, text: "Yes it is postponed, are you interested ? ", sender: "them"},
-        { id: 2, text: "Yes it is postponed, are you interested ? ", sender: "them"},
-        { id: 2, text: "Yes it is postponed, are you interested ?", sender: "me"},
-        { id: 2, text: "Yes it is postponed, are you interested ? ", sender: "me"},
-        { id: 2, text: "Yes it is postponed, are you interested ? ", sender: "me"},
-        { id: 2, text: "Yes it is postponed, are you interested ? ", sender: "them"},
-        { id: 2, text: "Yes it is postponed, are you interested ? ", sender: "them"},
-        { id: 2, text: "Yes it is postponed, are you interested ? ", sender: "them"},
-        { id: 2, text: "Yes it is postponed, are you interested ? ", sender: "them"},
-        { id: 5, text: "Yes it is postponed, are you interested ? ", sender: "them"},
-        { id: 3, text: "Yes i'm waiting for the new auction date ? ", sender: "them"},
-        { id: 10, text: "Yes i'm waiting for the new auction date ? ", sender: "me"}
-    ];
+    const apiPrivate = useApiPrivate();
+
+    const [messages, setMessages]  = useState<any[]>([]);
     
+    const [inputText, setInputText] = useState("");
+
+    const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+
+    const scrollToBottom = () => {
+        setTimeout(() => {
+            if (scrollContainerRef.current){
+                scrollContainerRef.current.scrollTo({
+                    top: scrollContainerRef.current.scrollHeight,
+                    behavior: "instant"
+                    }
+                )
+            }
+            
+        }, 10);
+
+
+    }
+    // SCROLLING TO THE BOTTOM WHEN CHANGE THE CHAT AND NEW MESSAGE
+    useEffect(() => scrollToBottom(), [messages, chatId])
+    
+    const handleSend = async () => {
+        if (inputText.trim() === "")
+            return ;
+        const newMessage = {
+            id: crypto.randomUUID(),
+            text: inputText,
+            sender: "me"
+        };
+        setMessages([...messages, newMessage]);
+        setInputText("");
+        try {
+            await apiPrivate.post(`/chat/send`, {
+                receiverId: chatId,
+                content: newMessage.text
+            });
+        } catch (error){
+            console.error("Failed to send message: ",  error);
+            // doing some disign for failed send
+        }
+    };
+
+
+    const { user } = useAuth();
+    useEffect(() => {
+        console.log(user);
+        setInputText("");
+        setMessages([]);
+        // TODO: TRIGGER AXIOS FETCH INBOX FOR THE NEW CHATID
+        const fetchHistory = async () => {
+            try {
+                const response = await apiPrivate.get(`/chat/history/${chatId}`);
+                const rawMessages = response.data.content;
+
+                const formattedMessages = rawMessages.map((dto:any) => ({
+
+                    id: dto.id,
+                    text: dto.content,
+                    sender: dto.senderId.toLowerCase() === user?.id ? "me" : "them"
+                }));
+                setMessages(formattedMessages.reverse());
+            } catch (error){
+                console.error("Failed to fetch chat history;", error);
+            }
+        }
+        fetchHistory();
+    }
+    , [chatId]);
+
+console.log('haha');
+
     return (
         <div className="flex flex-col w-full h-full bg-white">
             {/* // HEADER */}
@@ -36,8 +97,13 @@ function ChatWindow({ chatId } : {chatId:number}){
             </div>
 
             {/* // MESSAGE FEED  */}
-            <div className="flex-1 overflow-auto p-4 flex flex-col gap-4 bg-gray-50">
-                {fakeMessages.map((msg) => (
+            <div ref={scrollContainerRef} className="flex-1 overflow-auto p-4 flex flex-col gap-4 bg-gray-50
+            [scrollbar-width:thin]
+            [scrollbar-color:#E5E7EB_transparent]
+            hover:[scrollbar-color:#9CA3AF_transparent]
+            "
+            >
+                {messages?.map((msg) => (
                     <div 
                     key={msg.id}
                     className={`flex ${msg.sender === 'me' ? 'justify-end' : 'justify-start'}`}
@@ -51,7 +117,7 @@ function ChatWindow({ chatId } : {chatId:number}){
                             }`
                             }
                         >
-                            <p className="text-sm">{msg.text}</p>
+                            <p className="text-sm wrap-anywhere">{msg.text}</p>
                         </div>
                     </div>
                 )
@@ -60,17 +126,19 @@ function ChatWindow({ chatId } : {chatId:number}){
             {/* INPUT AREA */}
             <div className="p-4 bg-white border-t border-gray-200 shrink-0">
                 <div className="flex items-center gap-2">
-                    <input
+                    {/* TODO: CHANGE IT TO TEXT AREA FOR MULTIPLE PARAGRAPH SUPPORT */}
+                    <input 
                         type="text"
                         placeholder="Type amessage..."
+                        value={inputText}
+                        onChange={(e)=>setInputText(e.target.value)}
+                        onKeyDown={(e) => e.key === 'Enter' && handleSend()}
                         className="flex-1 px-4 py-2 bg-gray-100 border-transparent rounded-full  focus:outline-none focus:ring-2 focus:ring-blue-500 transition-transform"
                     />
-                    <button className="bg-blue-500 rounded-full px-4 py-2 text-white font-semibold hover:bg-blue-700 transition-colors flex items-center justify-center">
+                    <button className="bg-blue-500 rounded-full px-4 py-2 text-white font-semibold hover:bg-blue-700 transition-colors flex items-center justify-center"
+                            onClick={handleSend}>
                         Send
                     </button>
-                    {/* <button className="bg-blue-600 text-white font-semibold px-4 py-2 rounded-full hover:bg-blue-700 transition-colors flex items-center justify-center">
-                        Send
-                    </button> */}
 
                 </div>
             </div>
