d1efec1547480c448a92c08afd4333b37ef57900
anonbroski <idkfdsfdsfdsfdsfdsfdsfdsf.com>
2026-03-05 15:54:56 +0000

    feat: add markMeassageAsRead UPDATE query for setting up the isRead boolean

--- backend/chat-service/src/main/java/com/mazad/chat_service/repository/MessageRepository.java
+++ backend/chat-service/src/main/java/com/mazad/chat_service/repository/MessageRepository.java
@@ -9,7 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, UUID>
@@ -26,5 +28,10 @@ public interface MessageRepository extends JpaRepository<Message, UUID>
             ORDER BY timestamp DESC
             """, nativeQuery = true)
     Slice<Message> findInbox(@Param("userId") UUID userId, Pageable pageable);
-    
+   
+    @Modifying
+    @Transactional
+    @Query("UPDATE Message m SET m.isRead = true WHERE m.roomId = :roomId AND m.receiverId = :userId AND m.isRead = false")
+    void markMessageAsRead(@Param("roomId") String roomId, @Param("userId") UUID userId);
+
 }
