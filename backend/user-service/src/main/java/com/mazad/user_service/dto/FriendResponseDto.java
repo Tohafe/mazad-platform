71c288f56a556d77c41ec4e7eaa5664f9d6cb5c2
rekoune <rekoune123@gmail.com>
2026-03-07 13:02:12 +0000

    feat: add user id on FriendResponseDto

--- backend/user-service/src/main/java/com/mazad/user_service/dto/FriendResponseDto.java
+++ backend/user-service/src/main/java/com/mazad/user_service/dto/FriendResponseDto.java
@@ -1,10 +1,13 @@
 package com.mazad.user_service.dto;
 
+import java.util.UUID;
+
 import lombok.Builder;
 
 @Builder
 public record FriendResponseDto(
-        String username,
-        String thumbnail,
+        String  username,
+        String  thumbnail,
+        UUID    id,
         boolean onlineStatus
 ) {}
