f37177de7a29df81668da40c7850d64033b23576
AOUKY <aoukyhamza@gmail.com>
2026-03-08 17:35:30 +0000

    feat(websocket): add heartbeat to maintain connection

--- frontend/src/context/WebSocketContext.tsx
+++ frontend/src/context/WebSocketContext.tsx
@@ -30,7 +30,11 @@ export const WebSocketProvider = ({ children }: WebSocketProviderProps) => {
 
         const client = new Client({
             brokerURL: endpoint,
-            reconnectDelay: 5000, 
+            reconnectDelay: 5000,
+
+            heartbeatIncoming: 10000, 
+            heartbeatOutgoing: 10000,
+
             onConnect: () => {
                 console.log(`Global STOMP Engine Online! (Auth: ${accessToken ? 'YES' : 'NO'})`);
                 setIsConnected(true); 
@@ -38,6 +42,14 @@ export const WebSocketProvider = ({ children }: WebSocketProviderProps) => {
             onWebSocketClose: () => {
                 console.log("Connection lost");
                 setIsConnected(false); 
+            },
+
+            onWebSocketError: (error) => {
+                console.error("WebSocket Network Error:", error);
+            },
+            onStompError: (frame) => {
+                console.error("Broker Error:", frame.headers['message']);
+                console.error("Additional Details:", frame.body);
             }
         });
 
