a44fdd2eedcc518e7f8acdd9907c974db8cda452
AOUKY <aoukyhamza@gmail.com>
2026-03-08 17:14:09 +0000

    refactor: replace the svg with icon

--- frontend/src/components/Form/FilePreview.tsx
+++ frontend/src/components/Form/FilePreview.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import type { UploadableFile } from '../../types/upload';
+import { FiFile } from 'react-icons/fi';
+import React from 'react';
 
 interface FilePreviewProps {
     fileData: UploadableFile;
@@ -51,9 +52,7 @@ const FilePreview: React.FC<FilePreviewProps> = ({ fileData, onRemove, isMain = 
                         draggable={false}
                     />
                 ) : (
-                    <svg className="w-12 h-12 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
-                    </svg>
+                    <FiFile className="w-12 h-12 text-gray-400" />
                 )}
 
                 {fileData.status === 'UPLOADING' && (
