fe14b9403233904377450795cd58e7d8e27688f7
HamzaARS2 <hamza1arrastec@gmail.com>
2026-03-11 13:59:56 +0000

    fix(front): fix bug in CategoryCard specific for brave browser

--- frontend/src/components/Grid/CategoryGrid.tsx
+++ frontend/src/components/Grid/CategoryGrid.tsx
@@ -16,7 +16,7 @@ const CategoryGrid = ({className = "", categories, onCategoryClick, ...props}: C
     const baseStyles = "grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6";
 
     return (
-        <div className="flex flex-col gap-4">
+        <div className="flex w-full flex-col gap-4">
             <h2 className="text-base text-black font-semibold"></h2>
             <div className={cn(baseStyles, className)} {...props}>
                 {categories.map((category) => <CategoryCard key={category.id} onClick={() => onCategoryClick && onCategoryClick(category)}
