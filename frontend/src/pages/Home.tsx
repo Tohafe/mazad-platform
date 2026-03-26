5864f81e9b529385ce20577830b34bb683dba963
HamzaARS2 <hamza1arrastec@gmail.com>
2026-03-26 13:49:41 +0100

    feat(front): add status active to Home auctions hook

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,14 +8,14 @@ import {usePopularCategories} from "../hooks/useCategories.ts";
 const HomePageContent = () => {
     const {data: Categories = []} = usePopularCategories()
     const {data: EndingSoonAuctions = []} = useEndingSoonAuctions(128, 10);
-    const {data: extraAuctions} = useAuctions({page: 0, size: 20});
+    const {data: extraAuctions} = useAuctions({page: 0, size: 20, status: "ACTIVE"});
     const recentlyAdded = extraAuctions?.content.slice(0, 10) ?? []
     const mightLikeAuctions = extraAuctions?.content.slice(10, 20) ?? []
     return <div className="flex flex-col gap-10 items-center justify-center max-w-305  w-full py-6">
         <HeroCarousel/>
-        {EndingSoonAuctions &&
+        {EndingSoonAuctions.length > 0 &&
             <ItemCarousel auctions={EndingSoonAuctions} carouselTitle="Auctions ending soon"/>}
-        {Categories && <CategoryGrid categories={Categories}/>}
+        {Categories.length > 0 && <CategoryGrid categories={Categories}/>}
         {mightLikeAuctions.length > 0 && <ItemCarousel auctions={mightLikeAuctions} carouselTitle="You might also like"/>}
         {recentlyAdded.length > 0 && <ItemCarousel auctions={recentlyAdded} carouselTitle="Recently added"/>}
     </div>
