f3c376c77de2f3f7a3def0db7d1d9d288f9bf2d9
HamzaARS2 <hamza1arrastec@gmail.com>
2026-03-29 17:12:51 +0100

    feat(infra): add seeder script

---
+++ infrastructure/seeder/generate_item.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+
+DESCRIPTION="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+CATEGORY_ID=$(( (RANDOM % 13) + 1 ))
+
+CONDITIONS=("Mint Condition" "Rare" "Restored" "Brand New" "Vintage" "Antique")
+BRANDS=("Sony" "Rolex" "Samsung" "Artisan" "Yamaha" "Handmade")
+ITEMS=("Gaming Laptop" "Mechanical Keyboard" "Acoustic Guitar" "Diving Watch" "Persian Rug")
+EXTRAS=("in Original Box" "with Accessories" "- Limited Edition" "with Certificate")
+
+COND=${CONDITIONS[$RANDOM % ${#CONDITIONS[@]}]}
+BRAND=${BRANDS[$RANDOM % ${#BRANDS[@]}]}
+ITEM=${ITEMS[$RANDOM % ${#ITEMS[@]}]}
+EXTRA=${EXTRAS[$RANDOM % ${#EXTRAS[@]}]}
+
+TITLE="$COND $BRAND $ITEM $EXTRA"
+PRICE=$(( (RANDOM % 3000) + 100 ))
+
+THUMBNAIL="https://picsum.photos/seed/$RANDOM/600/450"
+
+HOURS=$(( (RANDOM % 60) + 1 ))
+
+SECONDS=$(( HOURS * 3600 ))
+
+FUTURE_TIME=$(( $(date +%s) + SECONDS ))
+
+ENDS_AT=$(date -u -d "@$FUTURE_TIME" +"%Y-%m-%dT%H:%M:%SZ")
+
+ITEM=$(cat <<EOF
+{
+    "categoryId": "$CATEGORY_ID",
+    "title": "$TITLE",
+    "description": "$DESCRIPTION",
+    "thumbnail": "$THUMBNAIL",
+    "images": [
+        "$THUMBNAIL",
+        "https://picsum.photos/seed/$RANDOM/600/450",
+        "https://picsum.photos/seed/$RANDOM/600/450",
+        "https://picsum.photos/seed/$RANDOM/600/450"
+    ],
+    "specs": {
+        "batch": "26",
+        "origin": "Collector Market",
+        "condition": "Excellent",
+        "authenticity_score": "98%",
+        "material": "Patinated Brass & Mahogany",
+        "production_year": 1942,
+        "weight":"1.2kg",
+        "width":"20cm",
+        "height":"15cm",
+        "rarity_index": "High",
+        "last_serviced": "2025-11-12"
+    },
+    "shippingInfo": "Worldwide shipping available.",
+    "startingPrice": "$PRICE",
+    "endsAt": "$ENDS_AT"
+}
+EOF
+)
+
+echo $ITEM
---
+++ infrastructure/seeder/seed.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USER1='{"username": "Amine_88", "email": "amine.dev@gmail.com", "password": "Password.123"}'
+USER2='{"username": "FatimaZ", "email": "fatima.z@gmail.com", "password": "Password.123"}'
+USER3='{"username": "Youssef_M", "email": "youssef.m@gmail.com", "password": "Password.123"}'
+USER4='{"username": "Sara.Ali", "email": "sara.ali.99@gmail.com", "password": "Password.123"}'
+USER5='{"username": "KarimTech", "email": "karim.tech@gmail.com", "password": "Password.123"}'
+USER6='{"username": "Nadia_Art", "email": "nadia.art@gmail.com", "password": "Password.123"}'
+
+USERS=("$USER1" "$USER2" "$USER3" "$USER4" "$USER5" "$USER6")
+
+echo "Registering Users..."
+
+for i in {0..5}; do
+  PAYLOAD="${USERS[$i]}"
+  RESPONSE=$(curl -k -s -X POST https://localhost:8443/api/v1/auth/register \
+    -H "Content-Type: application/json" \
+    -d "$PAYLOAD")
+done
+
+echo "Signing in users to get tokens..."
+
+for user in "${USERS[@]}"; do
+  LOGIN_RES=$(curl -k -s -X POST https://localhost:8443/api/v1/auth/login \
+          -H "Content-Type: application/json" \
+          -d "$user")
+  tokens+=("$(echo $LOGIN_RES | jq -r .accessToken)")
+done
+
+
+
+echo "Creating 10 items per user..."
+
+chmod +x generate_item.sh
+
+for token in "${tokens[@]}"; do
+  for i in {1..10}; do
+    ITEM=$(./generate_item.sh)
+
+    curl -k -s -X POST https://localhost:8443/api/v1/items \
+      -H "Content-Type: application/json" \
+      -H "Authorization: Bearer $token" \
+      -d "$ITEM" > /dev/null
+  done
+done
+
