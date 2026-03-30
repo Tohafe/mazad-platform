40302a92b704ab3fc284b5f6c96381321b7ad259
HamzaARS2 <hamza1arrastec@gmail.com>
2026-03-31 00:41:05 +0100

    fix(front): fix displaying not active auctions and support different screen sizes

--- frontend/src/components/Card/CategoryCard.tsx
+++ frontend/src/components/Card/CategoryCard.tsx
@@ -5,19 +5,29 @@ import {Link} from "react-router-dom";
 
 interface CategoryCardProps {
     className?: string;
+    titleClassName?: string;
+    imgClassName?: string;
     category: Category;
     onClick?: () => void;
 }
 
 
-const CategoryCard = ({className = "", category, onClick}: CategoryCardProps) => {
-    const baseStyles = "flex flex-row items-center justify-between aspect-5/2 p-2 md:px-4 cursor-pointer";
-    return <Link onClick={onClick} to={`/c/${category.id}-${category.slug}`} className={cn(baseStyles, className)}
-                style={{backgroundColor: category.hexColor}}>
-        <span className="flex-2 text-xs text-black h-full font-serif font-semibold md:text-lg">{category.name}</span>
-        <img className="w-8 h-8  md:w-16 md:h-16 object-cover rounded-full" loading="lazy" src={category.imageUrl}
-             alt="Not Found"/>
-    </Link>
+const CategoryCard = ({className = "", titleClassName = "", imgClassName = "", category, onClick}: CategoryCardProps) => {
+    const baseStyles = "flex flex-row items-center justify-between aspect-5/2 p-2 md:px-4 cursor-pointer overflow-hidden rounded-md"; // Added overflow-hidden and rounded-md for safety
+
+    return (
+        <Link onClick={onClick} to={`/c/${category.id}-${category.slug}`} className={cn(baseStyles, className)}
+              style={{backgroundColor: category.hexColor}}>
+
+            <span className={cn("flex-1 truncate mr-2 text-sm sm:text-lg text-black font-serif font-semibold md:text-lg", titleClassName)}>
+                {category.name}
+            </span>
+
+            <img className={cn("shrink-0 w-16 h-16 sm:w-20 sm:h-20 object-cover rounded-full", imgClassName)}
+                 loading="lazy" src={category.imageUrl}
+                 alt="Not Found"/>
+        </Link>
+    )
 }
 
 export default CategoryCard
