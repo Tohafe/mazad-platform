6fb9ff97d6b10915adc523ea26534453b0197f62
HamzaARS2 <hamza1arrastec@gmail.com>
2026-03-31 00:43:14 +0100

    fix(front): fix dialog categories images go outside the card

--- frontend/src/components/Dialog/CategoryDialog.tsx
+++ frontend/src/components/Dialog/CategoryDialog.tsx
@@ -6,6 +6,7 @@ import IconButton from "../Button/IconButton.tsx";
 import {MdClose} from "react-icons/md";
 import {useCategories} from "../../hooks/useCategories.ts";
 import type {Category} from "../../types/category.ts";
+import CategoryCard from "../Card/CategoryCard.tsx";
 
 interface CategoryDialogProps extends DialogHTMLAttributes<HTMLDialogElement> {
     classname?: string;
@@ -20,7 +21,7 @@ const CategoryDialog = ({className = "", dialogRef, onClose, ...props}: Category
         onClose();
     }
 
-    const baseStyles = "flex flex-col p-12 items-start justify-start w-fit h-fit bg-white m-auto backdrop:bg-black/75";
+    const baseStyles = "flex flex-col p-4 md:p-12 items-start justify-start w-fit h-fit bg-white m-auto backdrop:bg-black/75";
     return <dialog onClose={() => onClose()} ref={dialogRef} className={cn(baseStyles, className)} {...props} >
         <div className="flex flex-col items-center gap-4 w-full max-w-305">
             <div className="flex flex-row items-center justify-between gap-4 w-full">
@@ -28,7 +29,14 @@ const CategoryDialog = ({className = "", dialogRef, onClose, ...props}: Category
                 <IconButton onClick={() => onClose()} icon={MdClose} iconClassName="text-brand"/>
             </div>
 
-            {isLoading ? <div>Loading...</div> : <CategoryGrid onCategoryClick={handleCategoryClick} categories={categories} className="h-full"/>}
+            {isLoading ? <div>Loading...</div> : <CategoryGrid onCategoryClick={handleCategoryClick} className="h-full">
+                {categories && categories.map((category) => <CategoryCard
+                    titleClassName="text-xs"
+                    imgClassName="w-8 h-8"
+                    key={category.id}
+                    onClick={() => handleCategoryClick(category)}
+                    category={category}/>)}
+            </CategoryGrid>}
 
         </div>
     </dialog>
